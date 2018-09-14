ddc614ff3c67c8ba9cef15955ede2203cbccc0e0
Sébastien Geffroy <sebastien.geffroy@people-doc.com>
2018-09-14 14:39:28 +0200

    add tests for _extract_version()

--- west/__init__.py
+++ west/__init__.py
@@ -5,14 +5,7 @@ def _extract_version(package_name):
 
         return pkg_resources.get_distribution(package_name).version
     except pkg_resources.DistributionNotFound:
-        # Package is in the current directory or in the python path
-        from setuptools.config import read_configuration
-        from os import path as _p
-
-        _conf = read_configuration(
-            _p.join(_p.dirname(_p.dirname(__file__)), "setup.cfg")
-        )
-        return _conf["metadata"]["version"]
+        return "not_installed"
 
 
 __version__ = _extract_version("west")
