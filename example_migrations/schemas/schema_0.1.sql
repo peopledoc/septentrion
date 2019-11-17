1b0286ff167f5db388190a70f1889fdd28b2407d
Joachim Jablon <ewjoachim@gmail.com>
2019-11-17 10:57:00 +0100

    Fix strange punctuation choices

--- example_migrations/schemas/schema_0.1.sql
+++ example_migrations/schemas/schema_0.1.sql
@@ -2,8 +2,7 @@
 
 CREATE TABLE sql_version (
     version_num text UNIQUE NOT NULL
-)
-;
+);
 
 -- Init django tables --
 
@@ -13,35 +12,35 @@ CREATE TABLE "auth_permission" (
     "content_type_id" integer NOT NULL,
     "codename" varchar(100) NOT NULL,
     UNIQUE ("content_type_id", "codename")
-)
-;
+);
+
 CREATE TABLE "auth_group_permissions" (
     "id" serial NOT NULL PRIMARY KEY,
     "group_id" integer NOT NULL,
     "permission_id" integer NOT NULL REFERENCES "auth_permission" ("id") DEFERRABLE INITIALLY DEFERRED,
     UNIQUE ("group_id", "permission_id")
-)
-;
+);
+
 CREATE TABLE "auth_group" (
     "id" serial NOT NULL PRIMARY KEY,
     "name" varchar(80) NOT NULL UNIQUE
-)
-;
+);
+
 ALTER TABLE "auth_group_permissions" ADD CONSTRAINT "group_id_refs_id_f4b32aac" FOREIGN KEY ("group_id") REFERENCES "auth_group" ("id") DEFERRABLE INITIALLY DEFERRED;
 CREATE TABLE "auth_user_groups" (
     "id" serial NOT NULL PRIMARY KEY,
     "user_id" integer NOT NULL,
     "group_id" integer NOT NULL REFERENCES "auth_group" ("id") DEFERRABLE INITIALLY DEFERRED,
     UNIQUE ("user_id", "group_id")
-)
-;
+);
+
 CREATE TABLE "auth_user_user_permissions" (
     "id" serial NOT NULL PRIMARY KEY,
     "user_id" integer NOT NULL,
     "permission_id" integer NOT NULL REFERENCES "auth_permission" ("id") DEFERRABLE INITIALLY DEFERRED,
     UNIQUE ("user_id", "permission_id")
-)
-;
+);
+
 CREATE TABLE "auth_user" (
     "id" serial NOT NULL PRIMARY KEY,
     "password" varchar(128) NOT NULL,
@@ -54,8 +53,8 @@ CREATE TABLE "auth_user" (
     "is_staff" boolean NOT NULL,
     "is_active" boolean NOT NULL,
     "date_joined" timestamp with time zone NOT NULL
-)
-;
+);
+
 ALTER TABLE "auth_user_groups" ADD CONSTRAINT "user_id_refs_id_40c41112" FOREIGN KEY ("user_id") REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED;
 ALTER TABLE "auth_user_user_permissions" ADD CONSTRAINT "user_id_refs_id_4dc23c39" FOREIGN KEY ("user_id") REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED;
 -- The following references should be added but depend on non-existent tables:
@@ -75,15 +74,13 @@ CREATE TABLE "django_content_type" (
     "app_label" varchar(100) NOT NULL,
     "model" varchar(100) NOT NULL,
     UNIQUE ("app_label", "model")
-)
-;
+);
 
 CREATE TABLE "django_site" (
     "id" serial NOT NULL PRIMARY KEY,
     "domain" varchar(100) NOT NULL,
     "name" varchar(50) NOT NULL
-)
-;
+);
 
 CREATE TABLE django_migrations (
     "id" serial NOT NULL PRIMARY KEY,
